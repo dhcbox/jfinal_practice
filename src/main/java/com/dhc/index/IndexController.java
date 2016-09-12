4e0ebd85aaf6396914b8417004022dc937e55ca3
DHC <dhcbox@live.cn>
2016-09-12 14:52:59 +0800

    init

---
+++ src/main/java/com/dhc/index/IndexController.java
@@ -0,0 +1,15 @@
+package com.dhc.index;
+
+
+import com.jfinal.core.Controller;
+
+public class IndexController extends Controller {
+	public void index() {
+		render("index.html");
+	}
+}
+
+
+
+
+
